c5eed94059bc385f319d0de43955c9843118e8c4
thoaidm <minhthoai3@gmail.com>
2022-08-11 11:09:48 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          CHÀO MỪNG BÉ TOẠI ĐẾN VỚI ĐAM MÊ CHÂN CHÍNH
         </a>
       </header>
     </div>
